53fdc7dcbda328938bb1a785e5b1c4e048ed48a1
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-05-14 23:17:36 +0530

    notfollow

--- resources/views/errors/410.blade.php
+++ resources/views/errors/410.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta name="robots" content="noindex, follow">
+    <meta name="robots" content="noindex, nofollow">
+    
     <link rel="shortcut icon" href="{{asset('client/images/favicon.png')}}" type="image/png" />
     <title>@yield('title')</title>   
    @vite(['resources/css/app.css', 'resources/js/app.js'])    
