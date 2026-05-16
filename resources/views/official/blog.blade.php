fa5e42d1f373e7053879c19f3450682fe7482a02
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-05-16 18:38:29 +0530

    blog

--- resources/views/official/blog.blade.php
+++ resources/views/official/blog.blade.php
@@ -503,7 +503,7 @@
                             <path d="M20.59 13.41l-7.17 7.17a2 2 0 01-2.83 0L2 12V2h10l8.59 8.59a2 2 0 010 2.82z"/>
                             <line x1="7" y1="7" x2="7.01" y2="7"/>
                         </svg>
-                        Tags dd
+                        Tags
                     </h4>
                     <div class="flex flex-wrap gap-2">
                          @if($tags)
