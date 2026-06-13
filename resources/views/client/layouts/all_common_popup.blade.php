d0e84ccd173354415586f641164da1a1d3680b23
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-06-13 18:16:04 +0530

    form phone

--- resources/views/client/layouts/all_common_popup.blade.php
+++ resources/views/client/layouts/all_common_popup.blade.php
@@ -78,7 +78,11 @@ select.ef-input { padding-left:1rem; }
                 <div>
                     <label class="text-xs font-semibold text-gray-500 mb-1 block">Full Name *</label>
                     <div class="relative">
-                        <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none">👤</span>
+                        <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none">
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3.5 h-3.5">
+                                <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                            </svg>        
+                    </span>
                         <input type="text" name="name" placeholder="Enter full name" class="ef-input" required>
                     </div>
                 </div>
@@ -109,7 +113,11 @@ select.ef-input { padding-left:1rem; }
                             </div>
                         </div>
                         <div class="phone-input-wrap">
-                            <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" style="z-index:1;">📞</span>
+                            <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" style="z-index:1;">
+                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3.5 h-3.5">
+                    <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd"/>
+                </svg>
+                            </span>
                             <input type="tel" name="phone" placeholder="Enter phone number"
                                    class="ef-input" style="padding-left:2.25rem;" maxlength="16" required>
                         </div>
--- resources/views/client/layouts/enquiry-popup-form.blade.php
+++ resources/views/client/layouts/enquiry-popup-form.blade.php
@@ -90,7 +90,9 @@ select.ef-input { padding-left:1rem; }
                 <div>
                     <label class="text-xs font-semibold text-gray-500 mb-1 block">Full Name *</label>
                     <div class="relative">
-                        <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none">👤</span>
+                        <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3.5 h-3.5">
+                                <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                            </svg></span>
                         <input type="text" name="name" placeholder="Enter full name" class="ef-input" required>
                     </div>
                 </div>
@@ -126,7 +128,11 @@ select.ef-input { padding-left:1rem; }
 
                         {{-- Phone input --}}
                         <div class="phone-input-wrap">
-                            <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" style="z-index:1;">📞</span>
+                            <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" style="z-index:1;">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3.5 h-3.5">
+                                                <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd"/>
+                                            </svg>                           
+                            </span>
                             <input type="tel" name="phone" placeholder="Enter phone number"
                                    class="ef-input" style="padding-left:2.25rem;" maxlength="16" required>
 
@@ -157,11 +163,13 @@ select.ef-input { padding-left:1rem; }
             <div class="space-y-3">
 
                 {{-- Location --}}
-                {{-- Location --}}
+               
 <div>
     <label class="text-xs font-semibold text-gray-500 mb-1 block">Location</label>
     <div class="relative" id="popup-city-wrapper">
-        <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none z-10">📍</span>
+        <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none z-10"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3.5 h-3.5">
+                <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
+            </svg></span>
 
         <input type="text"
                id="popup-city-input"
