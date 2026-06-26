cf003af7e97ea3fb8d237eb527a19b2891886209
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-06-26 10:58:05 +0530

    anytics

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -50,15 +50,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KZF3WGSW');</script>
 <!-- End Google Tag Manager -->
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KF6W10RN9L"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-KF6W10RN9L');
-</script>
+ 
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 <style>
@@ -281,6 +273,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 ) !!}
 </script>
 @endif
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KF6W10RN9L"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KF6W10RN9L');
+</script>
 </head>
 <body class="min-h-screen bg-white text-gray-900 antialiased">
     <!-- Google Tag Manager (noscript) -->
