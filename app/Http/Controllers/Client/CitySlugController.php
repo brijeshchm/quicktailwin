2e3f52bb4bae161b36d6108f5793ee8973bc9f1b
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-09-15 11:55:15 +0530

    faq

--- app/Http/Controllers/Client/CitySlugController.php
+++ app/Http/Controllers/Client/CitySlugController.php
@@ -2304,6 +2304,22 @@ $reviewList = DB::table('clients')
             ->limit(50)
             ->get();
 
+   			$faqs = [];
+			$clientFaq = $clientsList['faqs'] ?? [];
+
+			if (!empty($clientFaq)) {
+				for ($i = 1; $i <= 10; $i++) {
+					$q = $clientFaq["faqq{$i}"] ?? null;
+					$a = $clientFaq["faqa{$i}"] ?? null;
+
+					if (!empty($q) && !empty($a)) {
+						$faqs[] = [
+							'q' => $q,
+							'a' => $a,
+						];
+					}
+				}
+			}
            
 
             // 🔹 Merge client data only when searching
@@ -2430,7 +2446,7 @@ $reviewList = DB::table('clients')
 
         return view('client.client-detail', compact(
             'slug', 'clientsList', 'keywordList','certificate','metaTitle','metaKeywords','metaDescription','keyword',
-            'comment', 'areaBusiness', 'overviewBusiness',
+            'comment', 'areaBusiness', 'overviewBusiness','faqs',
             'relatedList', 'gallery', 'hImages', 'vImages',
             'assignKeyword', 'certifications', 'govDocs', 'reviews',
             'gradients', 'bgColors', 'iconColors', 'planOptions',
