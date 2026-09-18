d86bdd12e174e4e419715a91a1208027fa78f876
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-09-18 18:34:23 +0530

    wen

--- routes/web.php
+++ routes/web.php
@@ -646,20 +646,7 @@ Route::POST('/client/lead/saveEnquiryContact', [App\Http\Controllers\Client\Home
 
 
 Route::POST('/lead/auto-form-save', [App\Http\Controllers\Client\HomePageController::class, 'autoFormSave']);
-//Route::POST('/{city}/lead/auto-form-save', [App\Http\Controllers\Client\HomePageController::class, 'autoFormSave']);
-
- 
-
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+  
 
 require __DIR__.'/auth.php';
 
